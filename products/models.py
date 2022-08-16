c5390968923a7359fa2a7304d5c8e3e24d87cf62
nepalimohan <nepalimohan@outlook.com>
2022-08-16 16:48:40 +0545

    SerializersAndModelsCreated

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
